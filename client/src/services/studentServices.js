933fd330f3c33a00f1f9d0a31d4449423f6b1c52
20120302 <khangnl24112002@gmail.com>
2023-06-09 19:50:08 +0700

    Add student services

--- client/src/services/studentServices.js
+++ client/src/services/studentServices.js
@@ -1,5 +1,23 @@
 import axiosClient from "../api/apiClient";
 
+export const studentServices = {
+  getListStudents: (id) => axiosClient.get(`students/getStudent?id=${id}`),
+  searchStudent: (id) =>
+    axiosClient.get(`students/getStudentSearch?studentName=${id}`),
+  getStudentsByClass: (name) =>
+    axiosClient.get(`students/getListClassStudents?name=${name}`),
+  updateScore: (id, params) =>
+    axiosClient.put(`scores/updateScore?id=${id}`, params),
+  getAVGStudentList: () => axiosClient.get(`scores/getAllStudentScore`),
+  getAVGStudent: (id) => axiosClient.get(`scores/avgScore?id=${id}`),
+  getAVGByCourse: (nameClass, courseName, semesterOne, semesterTwo) =>
+    axiosClient.get(
+      `scores/avgScoreByCourse?courseName=${courseName}&nameClass=${nameClass}&semesterOne=${semesterOne}&semesterTwo=${semesterTwo}`
+    ),
+  createStudent: (params) => axiosClient.post(`students/createStudent`, params),
+  deleteStudent: (id) => axiosClient.delete(`students/deleteStudent?id=${id}`),
+};
+
 export const classesServices = {
   getListClassesByGrades: (id) =>
     axiosClient.get(`classes/getListGradeClasses?name=${id}`),
