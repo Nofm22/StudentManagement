1fcd626df99ec11d9efd7f189b270d38b9500261
20120302 <khangnl24112002@gmail.com>
2023-06-09 19:49:35 +0700

    Add courses Services

--- client/src/services/studentServices.js
+++ client/src/services/studentServices.js
@@ -1,5 +1,20 @@
 import axiosClient from "../api/apiClient";
 
+export const coursesServices = {
+  createCourse: (params) => axiosClient.post(`courses/createCourse`, params),
+  getCoursesByGrade: (id) =>
+    axiosClient.get(`courses/getCoursesByGrade?gradeId=${id}`),
+  getCoursesSummary: (courseName, semesterOne, semesterTwo) =>
+    axiosClient.get(
+      `courses/getCoursesSummary?courseName=${courseName}&semesterOne=${semesterOne}&semesterTwo=${semesterTwo}`
+    ),
+  getSemesterSummary: (semesterOne, semesterTwo) =>
+    axiosClient.get(
+      `scores/getSemesterSummary?semesterOne=${semesterOne}&semesterTwo=${semesterTwo}`
+    ),
+  updateCourse: (params) => axiosClient.put(`courses/updateCourse`, params),
+};
+
 export const usersServices = {
   login: (params) => axiosClient.post(`users/login`, params),
   register: (params) => axiosClient.post(`users/register`, params),
