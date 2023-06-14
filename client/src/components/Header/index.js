d1d08acd2f189a4e441602629451e51c4028e488
20120302 <khangnl24112002@gmail.com>
2023-06-14 23:17:32 +0700

    Create component toogle to change theme

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,49 +4,44 @@ import styles from "./Header.module.sass";
 import { Link } from "react-router-dom";
 import Icon from "../Icon";
 import User from "./User";
+import Toogle from "../Toggle/Index";
 
 const Header = ({ onOpen, data }) => {
-    const [visible, setVisible] = useState(false);
-    const handleClick = () => {
-        onOpen();
-        setVisible(false);
-    };
-    const role = data.user?.role;
-    return (
-        <header className={styles.header}>
-            <button
-                className={styles.burger}
-                onClick={() => handleClick()}
-            ></button>
-            <button
-                className={cn(styles.buttonSearch, {
-                    [styles.active]: visible,
-                })}
-                onClick={() => setVisible(!visible)}
-            >
-                <Icon name="search" size="24" />
-            </button>
+  const [visible, setVisible] = useState(false);
+  const handleClick = () => {
+    onOpen();
+    setVisible(false);
+  };
+  const role = data.user?.role;
+  return (
+    <header className={styles.header}>
+      <Toogle />
+      <button className={styles.burger} onClick={() => handleClick()}></button>
+      <button
+        className={cn(styles.buttonSearch, {
+          [styles.active]: visible,
+        })}
+        onClick={() => setVisible(!visible)}
+      >
+        <Icon name="search" size="24" />
+      </button>
+      <div className={styles.control} onClick={() => setVisible(false)}>
+        {role === "admin" ? (
+          <>
+            {" "}
+            <Link className={cn("button", styles.button)} to="/sign-up">
+              <Icon name="add" size="24" />
+              <span>Create Account</span>
+            </Link>
+          </>
+        ) : (
+          <></>
+        )}
 
-            <div className={styles.control} onClick={() => setVisible(false)}>
-                {role === "admin" ? (
-                    <>
-                        {" "}
-                        <Link
-                            className={cn("button", styles.button)}
-                            to="/sign-up"
-                        >
-                            <Icon name="add" size="24" />
-                            <span>Create Account</span>
-                        </Link>
-                    </>
-                ) : (
-                    <></>
-                )}
-
-                <User role={role} className={styles.user} />
-            </div>
-        </header>
-    );
+        <User role={role} className={styles.user} />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ client/src/components/Toggle/Index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Toogle.scss";
+
+const Toggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode");
+    } else {
+      document.body.classList.add("light-mode");
+      document.body.classList.remove("dark-mode");
+    }
+  };
+
+  return (
+    <label className={`toggle ${isDarkMode ? "dark" : "light"}`}>
+      <input type="checkbox" checked={isDarkMode} onChange={handleToggle} />
+      <span className="slider" />
+    </label>
+  );
+};
+
+export default Toggle;
